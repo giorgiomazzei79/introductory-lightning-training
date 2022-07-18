cef69373231f9b06bc8511a1d4b16ea22891b8dc
Giorgio Mazzei <giorgio@fxdigital.uk>
2022-07-19 01:47:44 +0200

    feat: added buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-/*
- * If not stated otherwise in this file or this component's LICENSE file the
- * following copyright and licenses apply:
- *
- * Copyright 2020 Metrological
- *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { Lightning, Utils } from '@lightningjs/sdk'
 
+function getBackendData() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({ size: 6 })
+    }, 500)
+  })
+}
 export default class App extends Lightning.Component {
   static getFonts() {
     return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }]
@@ -26,46 +14,142 @@ export default class App extends Lightning.Component {
 
   static _template() {
     return {
+      w: 1920,
+      h: 1080,
+      rect: true,
+      color: 0xffff0000,
       Background: {
-        w: 1920,
-        h: 1080,
-        color: 0xfffbb03b,
-        src: Utils.asset('images/background.png'),
+        w: w => w,
+        h: h => h,
+        rect: true,
+        src: 'https://dummyimage.com/1920x1080/fff/aaa',
+        color: 0xff333333,
       },
-      Logo: {
-        mountX: 0.5,
-        mountY: 1,
-        x: 960,
-        y: 600,
-        src: Utils.asset('images/logo.png'),
-      },
-      Text: {
-        mount: 0.5,
-        x: 960,
-        y: 720,
-        text: {
-          text: "Let's start Building!",
-          fontFace: 'Regular',
-          fontSize: 64,
-          textColor: 0xbbffffff,
+      Container: {
+        w: w => w,
+        h: h => h,
+        rect: true,
+        color: 0xff003300,
+        flex: {
+          alignItems: 'center',
+          justifyContent: 'center',
+          direction: 'column',
+        },
+        Heading: {
+          text: {
+            text: 'test text',
+            fontSize: 60,
+            lineHeight: 90,
+          },
+        },
+        ButtonColumn: {
+          type: ButtonColumn,
+          size: 1,
         },
       },
     }
   }
 
+  _setup() {
+    const backendCall = async () => {
+      const response = await getBackendData()
+
+      this.tag('ButtonColumn').patch({ size: response.size })
+
+      console.log('response: ', response)
+    }
+
+    backendCall()
+  }
+
+  _getFocused() {
+    return this.tag('ButtonColumn')
+  }
+}
+
+class ButtonColumn extends Lightning.Component {
+  static _template() {
+    return {
+      flex: {
+        direction: 'column',
+      },
+    }
+  }
+
   _init() {
-    this.tag('Background')
-      .animation({
-        duration: 15,
-        repeat: -1,
-        actions: [
-          {
-            t: '',
-            p: 'color',
-            v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff46730 }, 0.8: { v: 0xfffbb03b } },
-          },
-        ],
-      })
-      .start()
+    this._focusIndex = 0
+    this._size = this.size
+
+    this.children = Array.from(Array(this.size)).map((x, index) => ({
+      type: Button,
+      flexItem: { marginTop: index !== 0 ? 20 : 0 },
+    }))
+  }
+
+  _getFocused() {
+    return this.children[this._focusIndex]
+  }
+
+  set size(value) {
+    console.log('value: ', value)
+    this._size = value
+    this.children = Array.from(Array(this._size)).map((x, index) => ({
+      type: Button,
+      flexItem: { marginTop: index !== 0 ? 20 : 0 },
+    }))
+  }
+
+  get size() {
+    return this._size
+  }
+
+  _handleDown() {
+    if (this._focusIndex < this.children.length - 1) {
+      this._focusIndex += 1
+    }
+  }
+
+  _handleUp() {
+    if (this._focusIndex > 0) {
+      this._focusIndex -= 1
+    }
+  }
+}
+
+class Button extends Lightning.Component {
+  static get squareSize() {
+    return 50
+  }
+
+  static _template() {
+    return {
+      w: Button.squareSize,
+      h: Button.squareSize,
+      rect: true,
+      color: 0xffffffff,
+    }
+  }
+
+  _focus() {
+    this.patch({
+      smooth: {
+        scale: 1.3,
+      },
+    })
+  }
+
+  _unfocus() {
+    this.patch({
+      smooth: {
+        scale: 1,
+      },
+    })
+  }
+
+  _handleEnter() {
+    this._selected = !this._selected
+    this.patch({
+      color: this._selected ? '0xffFF5733' : '0xffffffff',
+    })
   }
 }
